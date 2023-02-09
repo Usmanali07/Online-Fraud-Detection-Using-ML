66916698b93a228766df7f96c1520e18faf1de07
Usman Ali <74171156+Usmanali07@users.noreply.github.com>
2023-02-09 11:38:57 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -11,24 +11,37 @@ def predict(model , input_df):
 
 
 def run():
-    st.image("Untitled.png",caption='Virtual University Of Pakistan')
-
+    st.sidebar.image("Untitled.png",caption='Virtual University Of Pakistan')
     st.title("Online Payments Fraud Detection system")
-    st.subheader('Under the Supervision of Dr.  Mushtaq Hussain ')
-    
-    Type = st.number_input("Type" , min_value=0 , max_value=4 , value= 1)
-    
-    amount = st.number_input("Amount" , value= 100)
-    oldbalanceOrg= st.number_input("Initial balance before the transaction" , value= 1)
-    newbalanceOrig= st.number_input("Customer's balance after the transaction" , value= 1)
-    
+    st.sidebar.subheader('Under the Supervision of Dr.  Mushtaq Hussain ')
+    get= st.selectbox("Type",['CASH_OUT','CASH_IN','PAYMENT', 'TRANSFER','DEBIT'])
+    if get == 'CASH_OUT':
+        Type = 1
+    if get == 'CASH_IN':
+        Type = 2
+    if get == 'PAYMENT':
+        Type = 3
+    if get == 'TRANSFER':
+        Type = 4
+    if get == 'DEBIT':
+        Type = 5
+    amount = st.number_input("Amount" , value = 0)
+    oldbalanceOrg= st.number_input("Initial balance before the transaction" , value= 0)
+    newbalanceOrig= st.number_input("Customer's balance after the transaction" , value= 0)
     output = ""
     input_dict = {'type': Type , 'amount': amount , 'oldbalanceOrg':oldbalanceOrg,'newbalanceOrig':newbalanceOrig}
     input_df=pd.DataFrame([input_dict])
+    st.sidebar.bar_chart(input_df)
+    
     if st.button('Predict'):
         output=predict(model=saved_lr , input_df= input_df)
-        output='Prediction is   '+str(output)
+        if output == 1:
+            output='You do a Fraud Transaction'
+        if output == 0:
+            output = 'Your Transaction is Not Fraud'
+            
         
-    st.success('The output {}'.format(output))
+        
+    st.success(output)
 
 run()
